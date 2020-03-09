fb1c229da5bb1ba3478e029060c2709bd92d91cf
justin-wenning <wenning.justin@gmail.com>
2020-03-09 11:37:40 -0700

    Added middleware handler for requests to /api/products

--- server/index.js
+++ server/index.js
@@ -14,8 +14,20 @@ app.use(sessionMiddleware);
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
+  db.query('SELECT \'successfully connected\' AS "message"')
     .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
+app.get('/api/products', (req, res, next) => {
+  db.query(`
+    SELECT "productId",
+           "name",
+           "price",
+           "image",
+           "shortDescription"
+      FROM "products";
+  `).then(result => res.json(result.rows))
     .catch(err => next(err));
 });
 
