20719bf9fa0097f3e695eb5624db62e78e57902b
justin-wenning <wenning.justin@gmail.com>
2020-04-11 22:35:09 -0700

    Added a modal to cart summary module for removing items Added state property 'removing' to cart summary module Added method set removing to cart summary module Moved cart removal handling from cart summary item to cart summary

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 export default class CartSummary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      removing: null
+    };
+  }
+
+  setRemoving(productId) {
+    this.setState({ removing: productId });
+  }
+
   render() {
     const items = this.props.cart.map(item =>
       <CartSummaryItem
@@ -9,11 +20,48 @@ export default class CartSummary extends React.Component {
         item={item}
         setView={() => this.props.viewDetails({ productId: item.productId })}
         modifyInCart={(cartItemId, quantity) => this.props.modifyInCart(cartItemId, quantity)}
-        removeFromCart={productId => this.props.removeFromCart(productId)}/>
+        removeFromCart={productId => this.setRemoving(productId)}/>
     );
     const total = this.props.cart.reduce((a, b) => a + b.price * b.quantity, 0);
     return (
       <div className="container cart-summary">
+        {
+          <div
+            className="modal"
+            style={{ display: this.state.removing ? 'block' : 'none' }}>
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Are you sure you want to remove this item?</h5>
+                  <button
+                    type="button"
+                    className="close"
+                    onClick={() => this.setState({ removing: null })}>
+                    <span>&times;</span>
+                  </button>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                    onClick={() => this.setState({ removing: null })}>
+                      Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => {
+                      this.setState({ removing: null });
+                      this.props.removeFromCart(this.state.removing);
+                    }}>
+                      Remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
         <div className="row">
           <div className="col mb-3">
             <a onClick={this.props.viewCatalog} className="text-secondary">{'< Back to catalog'}</a>
@@ -43,7 +91,6 @@ export default class CartSummary extends React.Component {
             </div>
           </div>
         }
-
       </div>
     );
   }
