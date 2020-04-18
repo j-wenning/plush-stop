d1d8e0cde7d9990ce3ce1b5a8f134226d36ce490
justin-wenning <wenning.justin@gmail.com>
2020-04-18 13:01:22 -0700

    Added button to product list item Added add to cart method to product list item Updated button onclick to call add to cart method

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 
 export default class ProductListItem extends React.Component {
+  addToCart(e) {
+    e.stopPropagation();
+    this.props.addToCart();
+  }
+
   render() {
     return (
       <div className="col mb-4 product-list-item">
         <div onClick={this.props.setView} className="card">
           <img src={this.props.product.image} className="mt-1 card-img-top img-fluid" alt="" />
           <div className="card-body">
-            <h5 className="card-title">
-              {this.props.product.name}
-            </h5>
-            <h6 className="card-subtitle text-secondary">
-              ${(this.props.product.price / 100).toFixed(2)}
-            </h6>
+            <div className="row no-gutters">
+              <div className="col-8 no-gutters">
+                <h5 className="card-title">
+                  {this.props.product.name}
+                </h5>
+                <h6 className="card-subtitle text-secondary">
+                  ${(this.props.product.price / 100).toFixed(2)}
+                </h6>
+              </div>
+              <div className="d-flex col-4 no-gutters justify-content-end">
+                <button onClick={e => this.addToCart(e)} className="btn btn-success">
+                  <i className="fas fa-plus"/>
+                  <i className="fas fa-shopping-cart" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
