b9846dbd099f9d7a1c46356a0fd90dc87110e8d2
justin-wenning <wenning.justin@gmail.com>
2020-04-18 12:38:43 -0700

    Removed old product list item display properties Updated elements to comply with bootstrap standard

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,18 +3,17 @@ import React from 'react';
 export default class ProductListItem extends React.Component {
   render() {
     return (
-      <div onClick={this.props.setView} className="card product-list-item">
-        <div className="card-body">
-          <img src={this.props.product.image} className="card-img-top img-fluid" alt=""/>
-          <h5 className="card-title">
-            {this.props.product.name}
-          </h5>
-          <h6 className="card-subtitle text-secondary">
-            ${(this.props.product.price / 100).toFixed(2)}
-          </h6>
-          <p className="card-text">
-            {this.props.product.shortDescription}
-          </p>
+      <div className="col mb-4 product-list-item">
+        <div onClick={this.props.setView} className="card">
+          <img src={this.props.product.image} className="mt-1 card-img-top img-fluid" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">
+              {this.props.product.name}
+            </h5>
+            <h6 className="card-subtitle text-secondary">
+              ${(this.props.product.price / 100).toFixed(2)}
+            </h6>
+          </div>
         </div>
       </div>
     );
