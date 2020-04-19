1ac272bde74883018893b12be101a941e27a3f79
justin-wenning <wenning.justin@gmail.com>
2020-04-19 13:32:48 -0700

    Fixed header alignment for xsm screen sizes

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,12 +13,12 @@ export default class Header extends React.Component {
               style={{ filter: 'invert(100%)' }}
               alt=""
               className="d-none d-sm-inline-block align-text-bottom" />
-            <h2 className="col-11 d-inline-block align-text-bottom">Plush Stop</h2>
+            <h2 className="col-12 d-inline-block align-text-bottom">Plush Stop</h2>
           </a>
         </div>
         <div className="col-12 col-sm text-sm-right">
           <a onClick={this.props.viewCart} className="navbar-text">
-            <h2>
+            <h2 className="col-12">
               {this.props.cartItemCount} items
               <i className="fas fa-shopping-cart" />
             </h2>
