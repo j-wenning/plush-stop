55e9c53d1ae802269686396dd06a2b769c53e1db
justin-wenning <wenning.justin@gmail.com>
2020-04-19 12:36:39 -0700

    Updated empty href in header with view header prop method

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="navbar navbar-light bg-warning mb-4">
-        <a href="" className="navbar-brand col">
+        <a onClick={this.props.viewCatalog} className="navbar-brand col">
           <img
             src="./images/favicon.png"
             width="50"
@@ -14,7 +14,7 @@ export default class Header extends React.Component {
             className="d-none d-sm-inline-block align-text-bottom" />
           <h2 className="col-11 d-inline-block align-text-bottom">Plush Stop</h2>
         </a>
-        <a onClick={this.props.setView} className="col-12 col-sm">
+        <a onClick={this.props.viewCart} className="col-12 col-sm">
           <h2 className="col navbar-text text-sm-right">
             {this.props.cartItemCount} items
             <i className="fas fa-shopping-cart" />
