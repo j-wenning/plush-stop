f6d6d639384d36db0de5f3490b433cb9b3ced237
justin-wenning <wenning.justin@gmail.com>
2020-04-19 13:24:05 -0700

    Added sort functionality to product list view

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -4,7 +4,13 @@ import ProductListItem from './product-list-item';
 export default class ProductList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: false, message: '', products: [] };
+    this.state = {
+      error: false,
+      message: '',
+      products: [],
+      sortOpen: false,
+      sortType: 'name'
+    };
   }
 
   getProducts() {
@@ -15,6 +21,28 @@ export default class ProductList extends React.Component {
         error: true,
         message: err
       }));
+  }
+
+  toggleSort() {
+    this.setState({ sortOpen: !this.state.sortOpen });
+  }
+
+  sortProducts(method) {
+    this.setState({
+      products: this.state.products.sort((a, b) => {
+        switch (method) {
+          case 'name':
+            if (a.name.toUpperCase() === b.name.toUpperCase()) return 0;
+            else if (a.name.toUpperCase() < b.name.toUpperCase()) return -1;
+            return 1;
+          case 'price':
+            return a.price - b.price;
+          default:
+            return false;
+        }
+      }),
+      sortType: method
+    });
   }
 
   componentDidMount() {
@@ -31,12 +59,48 @@ export default class ProductList extends React.Component {
         })}
         addToCart={() => this.props.addToCart(p.productId)}/>
     );
+    const closeSort = this.state.sortOpen ? () => this.toggleSort() : () => {};
     return (
-      <div className="m-auto container row row-cols-1 row-cols-sm-2 row-cols-md-3 product-list">
-        {
-          this.state.error
-            ? <h2>Error: {this.state.message}</h2> : products
-        }
+      <div className="m-auto container" onClick={() => closeSort()}>
+        <div className="col">
+          <div className="row">
+            <h2>Sort by</h2>
+            <div className="dropdown col">
+              <button
+                className="btn btn-secondary dropdown-toggle text-capitalize"
+                type="button"
+                id="sortButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded={this.state.sortOpen.toString()}
+                onClick={() => this.toggleSort()}>
+                {this.state.sortType}
+              </button>
+              <div
+                className={`dropdown-menu m-0${this.state.sortOpen ? ' show' : ''}`}
+                id="sortDropdown"
+                aria-labelledby="sortButton"
+                style={{ top: 'unset', left: 'unset' }}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => this.sortProducts('name')}>
+                  Name
+                </button>
+                <button
+                  className="dropdown-item"
+                  onClick={() => this.sortProducts('price')}>
+                  Price
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 product-list">
+          {
+            this.state.error
+              ? <h2>Error: {this.state.message}</h2> : products
+          }
+        </div>
       </div>
     );
   }
