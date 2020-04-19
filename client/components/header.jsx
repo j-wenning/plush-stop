5bcb3a97156c3fd655ee161e14ed357663a3b896
justin-wenning <wenning.justin@gmail.com>
2020-04-19 12:42:12 -0700

    Updated containing tags in header so user won't be redirected upon clicking empty space in header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,22 +4,26 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="navbar navbar-light bg-warning mb-4">
-        <a onClick={this.props.viewCatalog} className="navbar-brand col">
-          <img
-            src="./images/favicon.png"
-            width="50"
-            height="50"
-            style={{ filter: 'invert(100%)' }}
-            alt=""
-            className="d-none d-sm-inline-block align-text-bottom" />
-          <h2 className="col-11 d-inline-block align-text-bottom">Plush Stop</h2>
-        </a>
-        <a onClick={this.props.viewCart} className="col-12 col-sm">
-          <h2 className="col navbar-text text-sm-right">
-            {this.props.cartItemCount} items
-            <i className="fas fa-shopping-cart" />
-          </h2>
-        </a>
+        <div className="col">
+          <a onClick={this.props.viewCatalog} className="navbar-brand">
+            <img
+              src="./images/favicon.png"
+              width="50"
+              height="50"
+              style={{ filter: 'invert(100%)' }}
+              alt=""
+              className="d-none d-sm-inline-block align-text-bottom" />
+            <h2 className="col-11 d-inline-block align-text-bottom">Plush Stop</h2>
+          </a>
+        </div>
+        <div className="col-12 col-sm text-sm-right">
+          <a onClick={this.props.viewCart} className="navbar-text">
+            <h2>
+              {this.props.cartItemCount} items
+              <i className="fas fa-shopping-cart" />
+            </h2>
+          </a>
+        </div>
       </div>
     );
   }
